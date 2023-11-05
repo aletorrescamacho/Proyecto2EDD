147edce1b794f8c366d0a36ce5c498c74e3c32e8
Luis <l.soriano@correo.unimet.edu.ve>
2023-11-05 17:00:39 -0400

    commit pa agarrar develop

--- src/Interfaz/Ventana13.java
+++ src/Interfaz/Ventana13.java
@@ -30,6 +30,8 @@ public class Ventana13 extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(50, 75, 149));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
